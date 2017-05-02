3d48aadcdf14656929c4a09ab2f88ad4be985e2d
Oyama87 <JustinTaylorG@Gmail.com>
2017-05-02 02:17:55 -0700

    update styling and abstracted buttons

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import BarButton from './BarButton';
 
 const ControlBar = () => {
   return (
     <View style={styles.barStyle}>
-      <View style={styles.leftBarStyle}>
-        <TouchableOpacity style={styles.buttonStyle}>
-          <Text style={styles.textStyle}>Main Menu</Text>
-        </TouchableOpacity>
       
-        <TouchableOpacity style={styles.buttonStyle}>
-          <Text style={styles.textStyle}>Last Card</Text>
-        </TouchableOpacity>
-      </View>
       
-      <View style={styles.rightBarStyle}>
-        <TouchableOpacity style={styles.buttonStyle}>
-          <Text style={styles.textStyle}>Submit Answer</Text>
-        </TouchableOpacity>
-      </View>
+        <BarButton buttonText={'Main Menu'} />
+        <BarButton buttonText={'Last Card'} />
+      
+      
+      
+        <BarButton buttonText={'Submit Answer'} />
+      
+    
     </View>
   );
 };
@@ -38,27 +34,6 @@ const styles = {
   },
   rightBarStyle: {
     alignItems: 'flex-end'
-  },
-  buttonStyle: {
-    borderRadius: 15,
-    borderWidth: 5,
-    borderColor: 'steelblue',
-    backgroundColor: 'steelblue',
-    marginLeft: 5,
-    marginRight: 5,
-    marginTop: 5,
-    marginBottom: 5,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textStyle: {
-    color: 'white',
-    fontSize: 15,
-    fontWeight: 'bold',
-    paddingBottom: 4,
-    paddingLeft: 4,
-    paddingRight: 4,
-    paddingTop: 4,
   }
 }
 

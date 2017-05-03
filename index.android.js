bad84ae6d12d6c4eed03326d1995ab47cce75296
Oyama87 <JustinTaylorG@Gmail.com>
2017-05-02 22:27:32 -0700

    Navigation implemented
    New function "navToggle" flips this.state.toggle between true and false, controlling the if statement that decides whether to render the MainMenu or BaseCard components.  navToggle is passed down as a prop as follows:
    flashCard -> MainMenu -> TouchableOpacity
    flashCard -> BaseCard -> ControlBar -> BarButton -> TouchableOpacity

--- index.android.js
+++ index.android.js
@@ -1,15 +1,44 @@
 import Expo from 'expo';
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
 import BaseCard from './src/components/BaseCard';
 import CardPrompt from './src/components/CardPrompt';
+import MainMenu from './src/components/MainMenu';
 
-const flashCard = () => {
-  return (
-    <View style={{flex: 1}}>
-      <BaseCard />
-    </View>
-  );
-};
+class flashCard extends Component {
+  constructor(props) {
+        super(props)
+        this.navToggle = this.navToggle.bind(this)
+        
+        this.state = {
+            start: false
+        };
+  }
+  
+  navToggle = () => {
+    this.setState({start: !this.state.start})
+    console.log(this.state)
+  }
+  
+  render() {  
+    if (this.state.start) 
+    { return (
+        <View style={{flex: 1}}>
+    <BaseCard onPress={this.navToggle} />
+        </View>
+      );
+     }    
+    else 
+    {
+      return (
+        <View style={{flex: 1}}>
+          <MainMenu onPress={this.navToggle} />
+        </View>
+      );  
+    }
+  }
+}
+
+
 
 Expo.registerRootComponent(flashCard);

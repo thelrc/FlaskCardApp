f033974653077bb4ea36fb194b0260b857ffa746
Oyama87 <JustinTaylorG@Gmail.com>
2017-05-23 06:04:47 -0700

    Final update before competition presentation

--- src/components/AnswerButton.js
+++ src/components/AnswerButton.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import  {TouchableOpacity, Text} from 'react-native';
+import React, { Component } from 'react';
+import  { TouchableOpacity, Text } from 'react-native';
 
 
 class AnswerButton extends Component {
@@ -22,14 +22,16 @@ class AnswerButton extends Component {
   render(props) {
     return (
       <TouchableOpacity 
-        style={[styles.unpressedStyle, this.props.borderState && styles.pressedStyle]} //This will overwrite unpressedStyle, if toggle is true
-        onPress={this.props.answer} > 
+        style={[styles.unpressedStyle, this.props.borderState && styles.pressedStyle]} 
+        //This will overwrite unpressedStyle, if toggle is true
+        onPress={this.props.answer} 
+      > 
           
           <Text style={styles.textStyle}>{this.props.answer_text}</Text>
       
       </TouchableOpacity>
     );
-  };
+  }
 }
           
 
@@ -37,29 +39,29 @@ const styles = {
   unpressedStyle: {
     flex: 1,
     borderRadius: 15,
-    borderWidth: 5,
-    borderColor: 'steelblue',
-    backgroundColor: 'black',
+    borderWidth: 3,
+    borderColor: 'white',
+    backgroundColor: '#355095',
     marginLeft: 5,
     marginRight: 5,
     justifyContent: 'center',
     alignItems: 'center'
   },
   pressedStyle: {
-    flex: 1,
-    borderRadius: 15,
-    borderWidth: 5,
-    borderColor: 'yellow',
-    backgroundColor: 'black',
-    marginLeft: 5,
-    marginRight: 5,
-    justifyContent: 'center',
-    alignItems: 'center'
+    //flex: 1,
+    //borderRadius: 15,
+    //borderWidth: 5,
+    borderColor: 'gold',
+    //backgroundColor: 'black',
+    //marginLeft: 5,
+    //marginRight: 5,
+    //justifyContent: 'center',
+    //alignItems: 'center'
   },
   textStyle: {
     fontWeight: 'bold',
     fontSize: 25,
-    color: 'royalblue'
+    color: 'white'
   }
 };
 

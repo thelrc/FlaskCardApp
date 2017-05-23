4f6711c43992d5315f2af90a7ce095dafe359d52
Oyama87 <JustinTaylorG@Gmail.com>
2017-05-23 06:05:26 -0700

    Final Update before competion presentation

--- src/components/AnswerOptions.js
+++ src/components/AnswerOptions.js
@@ -1,95 +1,32 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, View, Text} from 'react-native';
+import { TouchableOpacity, View, Text } from 'react-native';
 import AnswerButton from './AnswerButton';
 
 export default class AnswerOptions extends Component {
   
-  constructor() {
-      super()
-      
-      this.answerOne = this.answerOne.bind(this) 
-      this.answerTwo = this.answerTwo.bind(this)
-      this.answerThree = this.answerThree.bind(this)
-      this.answerFour = this.answerFour.bind(this)
+  constructor(props) {
 
-      
-      this.state = {
-          a1: false,
-          a2: false,
-          a3: false,
-          a4: false
-      }
-  }
-  
-  answerOne() {
-   if(this.state.a1 == true)
-    {
-      this.setState({a1: false, a2: false, a3: false, a4: false});
-      console.log('one');
-    }
-    else
-    {
-      this.setState({a1: true, a2: false, a3: false, a4: false});
-      console.log('one');
-    }
-  }
-  
-  answerTwo() {
-    if(this.state.a2 == true)
-    {
-      this.setState({a1: false, a2: false, a3: false, a4: false});
-      console.log('two');
-    }
-    else
-    {
-      this.setState({a1: false, a2: true, a3: false, a4: false});
-      console.log('two');
-    }
-  }
-  
-  answerThree() {
-    if(this.state.a3 == true)
-    {
-      this.setState({a1: false, a2: false, a3: false, a4: false});
-      console.log('three');
-    }
-    else
-    {
-      this.setState({a1: false, a2: false, a3: true, a4: false});
-      console.log('three');
-    }
-  }
-  
-  answerFour() {
-    if(this.state.a4 == true)
-    {
-      this.setState({a1: false, a2: false, a3: false, a4: false});
-      console.log('four');
-    }
-    else
-    {
-      this.setState({a1: false, a2: false, a3: false, a4: true});
-      console.log('four');
-    }
+      super(props);
   }
   
   render() {
+    
     return (
       <View style={styles.columnStyle}>
         
         <View style={styles.rowStyle}>
-          <AnswerButton answer_text='Option 1' answer={this.answerOne} borderState={this.state.a1} />
-          <AnswerButton answer_text='Option 2' answer={this.answerTwo} borderState={this.state.a2} />
+        <AnswerButton answer_text={this.props.deck[0].answer1} answer={this.props.answerOne} borderState={this.props.a1} />
+        <AnswerButton answer_text={this.props.deck[0].answer2} answer={this.props.answerTwo} borderState={this.props.a2} />
         </View>
         
         <View style={styles.rowStyle}>
-          <AnswerButton answer_text='Option 3' answer={this.answerThree} borderState={this.state.a3} /> 
-          <AnswerButton answer_text='Option 4' answer={this.answerFour} borderState={this.state.a4} />
+        <AnswerButton answer_text={this.props.deck[0].answer3} answer={this.props.answerThree} borderState={this.props.a3} /> 
+        <AnswerButton answer_text={this.props.deck[0].answer4} answer={this.props.answerFour} borderState={this.props.a4} />
         </View>
         
       </View>
     );
-  };
+  }
 }
 
 const styles = {
